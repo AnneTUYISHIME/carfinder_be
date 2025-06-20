5227fc08d97e2d6751fb5575263be923cc3d8fef
Tuyishime Anne <atuyishime28@gmail.com>
2025-06-20 12:35:20 +0300

    fix: require locks

---
+++ src/main/java/dev/as/carfinder/CarfinderApplication.java
@@ -0,0 +1,13 @@
+package dev.as.carfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfinderApplication.class, args);
+	}
+
+}
