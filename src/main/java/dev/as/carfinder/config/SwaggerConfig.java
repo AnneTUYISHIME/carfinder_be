3bd8b89a78d9d560d10fc92b85756d1521bd309f
Tuyishime Anne <atuyishime28@gmail.com>
2025-07-10 21:49:22 +0300

    refactor: add prod server

--- src/main/java/dev/as/carfinder/config/SwaggerConfig.java
+++ src/main/java/dev/as/carfinder/config/SwaggerConfig.java
@@ -23,6 +23,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 @Server(
                         description = "Local ENV",
                         url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://carfinder-894g.onrender.com"
                 )
         }
 )
